6cdf40bb1a0d92b3ddd19dc1b8f0e62935f3fbc4
kozeljko <ak6689@student.uni-lj.si>
2018-10-28 22:33:41 +0100

    updated artist service and resource

---
+++ api/src/main/java/si/fri/rso/team10/ArtistResource.java
@@ -0,0 +1,58 @@
+package si.fri.rso.team10;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@RequestScoped
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("artists")
+public class ArtistResource {
+
+    @Inject
+    private ArtistService artistService;
+
+    @GET
+    public Response getArtists() {
+        var artists = artistService.getArtists();
+        return Response.ok(artists).build();
+    }
+
+    @GET
+    @Path("{artistId}")
+    public Response getArtist(@PathParam("artistId") String artistId) {
+        try {
+            var artist = artistService.getArtist(Long.parseLong(artistId));
+            return Response.ok(artist).build();
+        } catch (NumberFormatException e) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+    @POST
+    public Response addArtist(Artist artist) {
+        if (artistService.addArtist(artist)) {
+            return Response.ok(artist).build();
+        } else {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+    @DELETE
+    @Path("{artistId}")
+    public Response deleteArtist(@PathParam("artistId") String artistId) {
+        try {
+            var artist = artistService.getArtist(Long.parseLong(artistId));
+            if(artist != null && artistService.deleteArtist(artist)){
+                return Response.ok().build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+        } catch (NumberFormatException e) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+}
--- services/src/main/java/si/fri/rso/team10/ArtistService.java
+++ services/src/main/java/si/fri/rso/team10/ArtistService.java
@@ -2,23 +2,46 @@ package si.fri.rso.team10;
 
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RequestScoped
-public class CatalogueService {
+public class ArtistService {
 
     @PersistenceContext
     private EntityManager em;
 
     public List<Artist> getArtists() {
-        return em.createQuery("select artist from Artist artist").getResultList();
+        return em.createQuery("select artist from Artist artist", Artist.class).getResultList();
+    }
+
+    public Artist getArtist(Long id) {
+        try {
+            return em.createQuery("select artist from Artist artist where artist.id = :id", Artist.class).setParameter("id", id).getSingleResult();
+        } catch (NoResultException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     public boolean addArtist(Artist artist) {
         try {
             beginTransaction();
             em.persist(artist);
+            commitTransaction();
+            return true;
+        } catch (Exception e) {
+            rollbackTransaction();
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean deleteArtist(Artist artist) {
+        try {
+            beginTransaction();
+            em.remove(artist);
             commitTransaction();
             return true;
         } catch (Exception e) {
