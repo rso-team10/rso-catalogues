13f38ed88921d2a9f02eb027a796c73ffea49a60
Nejc Kozlevčar <nejc.kozlevcar@solvera-lynx.com>
2019-01-06 00:44:06 +0100

    First version of fault tolerance
    need to check/implement open,closed,half-closed

--- services/src/main/java/si/fri/rso/team10/ArtistService.java
+++ services/src/main/java/si/fri/rso/team10/ArtistService.java
@@ -1,19 +1,36 @@
 package si.fri.rso.team10;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 @RequestScoped
-public class ArtistService extends AbstractService<Artist>{
+public class ArtistService extends AbstractService<Artist> {
 
     @PersistenceContext
     private EntityManager em;
 
+    @CircuitBreaker
+    @Timeout(value = 2, unit = ChronoUnit.SECONDS)
+    @Fallback(fallbackMethod = "getArtistsFallback")
     public List<Artist> getArtists() {
-        return em.createQuery("select artist from Artist artist", Artist.class).getResultList();
+        try {
+            TimeUnit.SECONDS.sleep(5); // just for demo fault tolerance
+            return em.createQuery("select artist from Artist artist", Artist.class).getResultList();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public Artist getArtist(Long id) {
@@ -23,6 +40,11 @@ public class ArtistService extends AbstractService<Artist>{
             System.out.println(e.getMessage());
             return null;
         }
+    }
+
+    public List<Artist> getArtistsFallback() {
+        Logger.getLogger(Artist.class.getName()).info("Fallback");
+        return Collections.emptyList();
     }
 
     @Override
